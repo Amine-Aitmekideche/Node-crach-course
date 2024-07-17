a09cc4611d152f2ac85870abf43f53cdcd925abd
Ait mekideche mohammed Amine <lm_aitmekideche@esi.dz>
2024-07-17 16:44:54 +0100

    request obj + response obj + send html page + basic routing + redirects

--- serveur.js
+++ serveur.js
@@ -1,7 +1,46 @@
 const http = require('http')
+const fs = require('fs')
 
 const serveur = http.createServer((req, res) =>{
- console.log('requist made')
+ console.log(req.url,req.method)
+
+ //set header response content type
+ res.setHeader('Content-Type','text/html')
+ // WHITOUT FILE HTML
+//  res.write('<head><link rel="StyleSheet" href="#"></head>')  
+//  res.write('<p>hello words!</p>')
+//  res.write('<p>hello again words!</p>')
+//  res.end()
+let path = './views/';
+
+switch(req.url){
+    case '/':
+        path += 'index.html';
+        res.statusCode= 200;
+        break;
+    case '/about':
+        path += 'about.html';
+        res.statusCode= 200;
+        break;
+    case '/about-me':
+        res.statusCode= 301;//redirct
+        res.setHeader('Location','/about');
+        res.end();
+        break;
+    default:
+        path += '404.html';
+        res.statusCode= 404;
+        break;
+}
+fs.readFile(path,(err,data) =>{
+    if(err){
+        console.log('there is an erreur')
+        res.end();
+    }
+    // res.write(data)
+    res.end(data)
+})
+
 })
 // (req, res) => { ... } : Il s'agit d'une fonction de rappel (callback) qui sera exécutée chaque fois qu'une requête HTTP est reçue par le serveur.
 
